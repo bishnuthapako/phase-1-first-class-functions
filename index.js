584d27b80d38897747fd1973d41d845b1bda4857
Bishnu <bishnuthapako@gmail.com>
2022-03-27 18:11:16 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+  return callback();
+} 
+console.log(receivesAFunction("Hello, Good Morning"))
+
+
+function returnsANamedFunction(){
+  return returnsANamedFunction;
+  
+}
+
+function returnsAnAnonymousFunction(){
+  return function(name){
+    return name
+  }
+}
